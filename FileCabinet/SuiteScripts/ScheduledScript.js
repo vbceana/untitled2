774b414a2bd86d56e61be480c22e973ae1f5f3cb
Vin Battad <vbattad@ceanatech.com>
2022-04-18 14:09:40 +0800

    Initial commit

---
+++ FileCabinet/SuiteScripts/ScheduledScript.js
@@ -0,0 +1,24 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ScheduledScript
+ */
+define(['N/record', 'N/search'],
+    /**
+ * @param{record} record
+ * @param{search} search
+ */
+    (record, search) => {
+
+        /**
+         * Defines the Scheduled script trigger point.
+         * @param {Object} scriptContext
+         * @param {string} scriptContext.type - Script execution context. Use values from the scriptContext.InvocationType enum.
+         * @since 2015.2
+         */
+        const execute = (scriptContext) => {
+
+        }
+
+        return {execute}
+
+    });
